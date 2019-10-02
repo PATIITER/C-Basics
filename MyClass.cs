2f72634cca94e0374438167c62869b53a03b685e
PATTER <patercup@gmail.com>
2019-10-02 12:20:16 +0700

    update

---
+++ MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C_Basics
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace C_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            int num;
+            Console.Write("input :");
+            int.TryParse(Console.ReadLine(),out num); 
+            int score;
+            int count =1;
+            switch(num)
+            {
+                    case 1:
+                    Console.Write("input :");
+                    int.TryParse(Console.ReadLine(),out score); 
+
+                    while(count<=score)
+                        {
+                            Console.WriteLine("WTF " +count);
+                            count++;
+                        } break;
+                    case 2:
+                    for(int i=10;i>=0;i--)
+                        {
+                             Console.WriteLine("WTF = " +i);
+                        }break;
+                    case 3:
+                        do
+                        {
+                            Console.Write("input :");
+                            int.TryParse(Console.ReadLine(),out score);   
+
+
+                        }while(score >0);break;
+
+
+            }
+          
+             
+
+          
+
+             
+
+                  
+        }
+    }
+}
